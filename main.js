83a3e0809171e340b6c2c697e4c736040b0bd3e6
simoncarda <165000637+simoncarda@users.noreply.github.com>
2024-12-19 20:03:19 +0100

    s

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ const statsHrac = {jmeno: "hrac1", zivoty: 3, penezenka: 0, cenaZivota: 0, sazka
 
 function naplnitPenezenku() {
     let input = Number(document.getElementById("inputPenezenka").value);
-
-    aktualizovat();
-
     if (input>0) {
         statsHrac.penezenka += input;
         statsHrac.cenaZivota += input/3;
@@ -13,10 +10,31 @@ function naplnitPenezenku() {
         document.getElementById("tlacitkoPenezenka").disabled = true;
 
         document.getElementById("inputSazka").disabled = false;
-        document.getElementById("tlacitkoSazka").disabled = false;       
+        document.getElementById("tlacitkoSazka").disabled = false;
+        aktualizovat();    
     } else{
         alert("Tímto svou peněženku nenaplníš!")
     }
+}
+function vsadit() {
+    let input = document.getElementById("inputSazka").value;
+    if (input <= statsHrac.penezenka && input > 0) {
+        statsHrac.sazka = input;
+        statsHrac.penezenka -= statsHrac.sazka;
+        document.getElementById("inputSazka").disabled = true;
+        document.getElementById("tlacitkoSazka").disabled = true;
+        document.getElementById("vzitKartu").disabled = false;
+        aktualizovat();
+
+/*         novyBalicek();
+        rozdatDveKarty();
+        aktualizovatHodnoty(); */
+    } else{
+        alert("Toto vsadit nemůžeš!");
+    }
+}
+function novyBalicek(params) {
+    
 }
 
 function aktualizovat() {
@@ -30,4 +48,6 @@ function aktualizovat() {
         ctx.stroke();
         ctx.fill();   
     }
+    document.getElementById("infoPenezenka").innerHTML = "Peněženka: " + statsHrac.penezenka;
+    document.getElementById("infoSazka").innerHTML = "Sázka: " + statsHrac.sazka;
 }
